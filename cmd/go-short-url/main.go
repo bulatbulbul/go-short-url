10fcf9570e506c58943196504af9b78642d83699
Bulat Saifullin <bulatsajfullin00@gmail.com>
2025-09-29 23:50:37 +0300

    Creating and launching an HTTP server

--- cmd/go-short-url/main.go
+++ cmd/go-short-url/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/fatih/color"
 	"go-short-url/internal/http-server/handlers/url/save"
 	"log/slog"
+	"net/http"
 	"os"
 
 	"github.com/go-chi/chi/v5"
@@ -45,8 +46,22 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Post("/url", save.New(log, storage))
-	// TODO run server
 
+	log.Info("starting server", slog.String("address", cfg.Address))
+
+	srv := &http.Server{
+		Addr:         cfg.Address,
+		Handler:      router,
+		ReadTimeout:  cfg.HTTPServer.Timeout,
+		WriteTimeout: cfg.HTTPServer.Timeout,
+		IdleTimeout:  cfg.HTTPServer.IdleTimeout,
+	}
+
+	if err = srv.ListenAndServe(); err != nil {
+		log.Error("failed to start server")
+	}
+
+	log.Error("server stopped")
 }
 
 func setupLogger(env string) *slog.Logger {
